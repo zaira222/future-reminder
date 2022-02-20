2267d91a473610934e9ce4abe116b7cb4cb92e51
ZP <zairaperez01@gmail.com>
2022-02-20 02:30:37 -0600

    added homepage

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const PORT = process.env.PORT || 3001;
+const  notes  = require('./db/db.json');
+const ShortUniqueId = require('short-unique-id');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname, '/index.html');
+});
+
